af9b9d6c83636369ccf9324d63495c7565fb27c9
Bartosz Żabiński <bartosz.zabinski@student.put.poznan.pl>
2017-01-30 04:40:36 +0100

    initial commit with model

---
+++ tvshows/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from models import Show,Category
+# Register your models here.
+
+# Admin panel superuser:
+# U : admin
+# P : zaq12wsx
+
+def make_published(modeladmin, request, queryset):
+    queryset.update(accepted='True')
+make_published.short_description = "Approve selected TV Shows"
+
+class ShowAdmin(admin.ModelAdmin):
+    fields = ['accepted','image', 'title','slug','description', 'category' ,'status', 'aired_from', 'aired_to']
+    list_display = ['accepted', 'title', 'get_category' ,'status', 'aired_from', 'aired_to']
+    list_filter = ['status', 'category', 'accepted']
+    list_editable = ['title', 'status']
+    prepopulated_fields = {'slug': ('title',)}
+    actions = [make_published]
+
+admin.site.register(Show, ShowAdmin)
+
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Category, CategoryAdmin)
---
+++ tvshows/models.py
@@ -0,0 +1,52 @@
+from __future__ import unicode_literals
+
+from django.utils import timezone
+
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Show(models.Model):
+    accepted = models.BooleanField(default=False)
+    title = models.CharField(max_length=250, db_index=True)
+    description = models.TextField(blank=True,null=True)
+    category = models.ManyToManyField('tvshows.Category', related_name='shows')
+    slug = models.SlugField(max_length=250, db_index=True,unique=True)
+    image = models.ImageField(upload_to='cover/%Y/%m/%d', blank=True)
+    AIR_STATUS = (
+        (1, 'current show'),
+        (2, 'ended / cancelled'),
+        (3, 'on hiatus'),
+    )
+    status = models.IntegerField(default=1, choices=AIR_STATUS)
+    aired_from = models.DateField(default=timezone.now)
+    aired_to = models.DateField(blank=True,null=True)
+    updated = models.DateField(auto_now=True)
+
+    def get_category(self):
+        return "\n, ".join([c.name for c in self.category.all()])
+
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, db_index=True)
+    slug = models.SlugField(max_length=50, db_index=True, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+class Comment(models.Model):
+    accepted = models.BooleanField(default=False)
+    tvshow_id = models.ForeignKey('tvshows.Show', related_name='comments')
+    author = models.ForeignKey(User)
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
